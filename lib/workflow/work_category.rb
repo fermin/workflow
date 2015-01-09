c24831014f6646d3ccf201fa963968ad6eef7d8e
fermin <sufeng00@gmail.com>
2015-01-09 12:20:39 +0800

    ..

--- lib/workflow/work_category.rb
+++ lib/workflow/work_category.rb
@@ -3,6 +3,9 @@ class WorkCategory < ActiveRecord::Base
 
   has_many :work_items
   has_many :work_events
+
+  validates :name, :column_name, presence: true
+  validates_uniqueness_of :name, scope: [:column_name]
   
   def self.column_names_with_table
     self.column_names.map { |c| "#{table_name}.#{c}" }
